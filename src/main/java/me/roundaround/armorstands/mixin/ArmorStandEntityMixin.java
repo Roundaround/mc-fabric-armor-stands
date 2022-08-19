ca9579e71d101443a416f228a83098038178898c
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-08-19 13:26:13 -0400

    Improved interact behavior

--- src/main/java/me/roundaround/armorstands/mixin/ArmorStandEntityMixin.java
+++ src/main/java/me/roundaround/armorstands/mixin/ArmorStandEntityMixin.java
@@ -1,6 +1,7 @@
 package me.roundaround.armorstands.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -8,16 +9,37 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import me.roundaround.armorstands.client.hooks.ClientHooks;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
 
 @Mixin(ArmorStandEntity.class)
 public abstract class ArmorStandEntityMixin {
-  @Inject(method = "interactAt", at = @At(value = "HEAD"))
-  public void interactAt(PlayerEntity player, Vec3d hitPos, Hand hand, CallbackInfoReturnable<ActionResult> info) {
+  @Shadow
+  public abstract boolean isMarker();
+
+  @Inject(method = "interactAt", at = @At(value = "HEAD"), cancellable = true)
+  public void interactAt(
+      PlayerEntity player,
+      Vec3d hitPos,
+      Hand hand,
+      CallbackInfoReturnable<ActionResult> info) {
+    ItemStack itemStack = player.getStackInHand(hand);
+    if (isMarker() || itemStack.isOf(Items.NAME_TAG)) {
+      info.setReturnValue(ActionResult.PASS);
+      return;
+    }
+    if (player.isSpectator()) {
+      info.setReturnValue(ActionResult.SUCCESS);
+      return;
+    }
     if (player.world.isClient) {
       ClientHooks.openArmorStandScreen((ArmorStandEntity) (Object) this);
+      info.setReturnValue(ActionResult.CONSUME);
+      return;
     }
+    info.setReturnValue(ActionResult.PASS);
   }
 }
---
+++ src/main/java/me/roundaround/armorstands/mixin/NameTagItemMixin.java
@@ -0,0 +1,29 @@
+package me.roundaround.armorstands.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.NameTagItem;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+
+@Mixin(NameTagItem.class)
+public abstract class NameTagItemMixin {
+  @Inject(method = "useOnEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setCustomName(Lnet/minecraft/text/Text;)V"), cancellable = true)
+  public void useOnEntity(
+      ItemStack stack,
+      PlayerEntity player,
+      LivingEntity entity,
+      Hand hand,
+      CallbackInfoReturnable<ActionResult> info) {
+    if (entity instanceof ArmorStandEntity) {
+      entity.setCustomNameVisible(true);
+    }
+  }
+}
