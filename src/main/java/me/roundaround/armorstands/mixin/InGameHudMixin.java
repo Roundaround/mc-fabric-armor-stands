d8330787ed31ab3876dd337016e310776c6b143b
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-08-20 00:48:40 -0400

    Hide in-game HUD when armor stand screen open

---
+++ src/main/java/me/roundaround/armorstands/mixin/InGameHudMixin.java
@@ -0,0 +1,25 @@
+package me.roundaround.armorstands.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import me.roundaround.armorstands.client.gui.screen.AbstractArmorStandScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+  @Shadow
+  private MinecraftClient client;
+
+  @Inject(method = "render", at = @At(value = "HEAD"), cancellable = true)
+  private void render(MatrixStack matrixStack, float delta, CallbackInfo info) {
+    if (client.currentScreen instanceof AbstractArmorStandScreen) {
+      info.cancel();
+    }
+  }
+}
