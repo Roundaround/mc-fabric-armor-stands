22fb743d766adf22d9c194e37ee122439c138837
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-08-22 22:50:30 -0400

    Broken but moving to other computer

--- src/main/java/me/roundaround/armorstands/screen/ArmorStandScreenHandler.java
+++ src/main/java/me/roundaround/armorstands/screen/ArmorStandScreenHandler.java
@@ -40,8 +40,75 @@ public class ArmorStandScreenHandler extends ScreenHandler {
   }
 
   @Override
-  public ItemStack transferSlot(PlayerEntity player, int slot) {
+  public ItemStack transferSlot(PlayerEntity player, int index) {
+    if (index < 0 || index >= slots.size()) {
+      return ItemStack.EMPTY;
+    }
+
+    Slot slot = slots.get(index);
+
+    if (slot == null || !slot.hasStack()) {
+      return ItemStack.EMPTY;
+    }
+
+    // 0-8 is hotbar
+    // 9-35 is main player inventory
+    // 36-39 is equipment
+    // 40-41 is hands
+
+    ItemStack stack = slot.getStack();
+    ItemStack originalStack = stack.copy();
+
+    if (index < PlayerInventory.MAIN_SIZE) {
+      EquipmentSlot equipmentSlot = ArmorStandEntity.getPreferredEquipmentSlot(stack);
+
+      if (equipmentSlot.getType().equals(EquipmentSlot.Type.ARMOR)) {
+        
+      }
+      
+      return ItemStack.EMPTY;
+    }
+
     return ItemStack.EMPTY;
+
+    ItemStack itemStack = ItemStack.EMPTY;
+    Slot slot = (Slot) slots.get(index);
+    if (slot != null && slot.hasStack()) {
+      int i;
+      ItemStack itemStack2 = slot.getStack();
+      itemStack = itemStack2.copy();
+      EquipmentSlot equipmentSlot = ArmorStandEntity.getPreferredEquipmentSlot(itemStack);
+      if (index == 0) {
+        if (!this.insertItem(itemStack2, 9, 45, true)) {
+          return ItemStack.EMPTY;
+        }
+        slot.onQuickTransfer(itemStack2, itemStack);
+      } else if (index >= 1 && index < 5 ? !this.insertItem(itemStack2, 9, 45, false)
+          : (index >= 5 && index < 9 ? !this.insertItem(itemStack2, 9, 45, false)
+              : (equipmentSlot.getType() == EquipmentSlot.Type.ARMOR
+                  && !((Slot) this.slots.get(8 - equipmentSlot.getEntitySlotId())).hasStack()
+                      ? !this.insertItem(itemStack2, i = 8 - equipmentSlot.getEntitySlotId(), i + 1, false)
+                      : (equipmentSlot == EquipmentSlot.OFFHAND && !((Slot) this.slots.get(45)).hasStack()
+                          ? !this.insertItem(itemStack2, 45, 46, false)
+                          : (index >= 9 && index < 36 ? !this.insertItem(itemStack2, 36, 45, false)
+                              : (index >= 36 && index < 45 ? !this.insertItem(itemStack2, 9, 36, false)
+                                  : !this.insertItem(itemStack2, 9, 45, false))))))) {
+        return ItemStack.EMPTY;
+      }
+      if (itemStack2.isEmpty()) {
+        slot.setStack(ItemStack.EMPTY);
+      } else {
+        slot.markDirty();
+      }
+      if (itemStack2.getCount() == itemStack.getCount()) {
+        return ItemStack.EMPTY;
+      }
+      slot.onTakeItem(player, itemStack2);
+      if (index == 0) {
+        player.dropItem(itemStack2, false);
+      }
+    }
+    return itemStack;
   }
 
   @Override
