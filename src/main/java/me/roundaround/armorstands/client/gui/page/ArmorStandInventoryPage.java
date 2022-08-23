e29d5e980108878afa9f0c2b0df5e9e94cb429cb
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-08-23 10:40:23 -0400

    Made own renderEntity method

--- src/main/java/me/roundaround/armorstands/client/gui/page/ArmorStandInventoryPage.java
+++ src/main/java/me/roundaround/armorstands/client/gui/page/ArmorStandInventoryPage.java
@@ -5,10 +5,18 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import me.roundaround.armorstands.ArmorStandsMod;
 import me.roundaround.armorstands.client.ArmorStandsClientMod;
 import me.roundaround.armorstands.client.gui.screen.ArmorStandScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GraphicsMode;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.render.DiffuseLighting;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Quaternion;
+import net.minecraft.util.math.Vec3f;
 
 public class ArmorStandInventoryPage extends AbstractArmorStandPage {
   private static final int BACKGROUND_WIDTH = 176;
@@ -50,12 +58,68 @@ public class ArmorStandInventoryPage extends AbstractArmorStandPage {
         BACKGROUND_WIDTH,
         BACKGROUND_HEIGHT);
 
-    InventoryScreen.drawEntity(
-        x + 87,
-        y + 75,
-        30,
-        0f,
-        0f,
-        screen.getArmorStand());
+    drawEntity(x + 86, y + 75, 30, 0f, 0f);
+  }
+
+  public void drawEntity(int x, int y, int size, float mouseX, float mouseY) {
+    ArmorStandEntity entity = screen.getArmorStand();
+    MinecraftClient client = MinecraftClient.getInstance();
+    EntityRenderDispatcher entityRenderDispatcher = client.getEntityRenderDispatcher();
+
+    MatrixStack matrixStack = RenderSystem.getModelViewStack();
+    matrixStack.push();
+    matrixStack.translate(x, y, 1050f);
+    matrixStack.scale(1f, 1f, -1f);
+    RenderSystem.applyModelViewMatrix();
+
+    MatrixStack matrixStack2 = new MatrixStack();
+    matrixStack2.translate(0f, 0f, 1000f);
+    matrixStack2.scale(size, size, size);
+    Quaternion quaternion = Vec3f.POSITIVE_Z.getDegreesQuaternion(180f);
+    Quaternion quaternion2 = Vec3f.POSITIVE_X.getDegreesQuaternion(0f);
+    quaternion.hamiltonProduct(quaternion2);
+    matrixStack2.multiply(quaternion);
+
+    float bodyYaw = entity.bodyYaw;
+    float yaw = entity.getYaw();
+    boolean name = entity.isCustomNameVisible();
+    entity.bodyYaw = 180f;
+    entity.setYaw(180f);
+    entity.setCustomNameVisible(false);
+
+    DiffuseLighting.method_34742();
+    quaternion2.conjugate();
+    entityRenderDispatcher.setRotation(quaternion2);
+    entityRenderDispatcher.setRenderShadows(false);
+    VertexConsumerProvider.Immediate immediate = client
+        .getBufferBuilders()
+        .getEntityVertexConsumers();
+    runInFancyGraphicsMode(() -> {
+      entityRenderDispatcher.render(entity, 0, 0, 0, 0f, 1f, matrixStack2, immediate, 0xF000F0);
+    });
+    immediate.draw();
+    entityRenderDispatcher.setRenderShadows(true);
+
+    entity.bodyYaw = bodyYaw;
+    entity.setYaw(yaw);
+    entity.setCustomNameVisible(name);
+
+    matrixStack.pop();
+    RenderSystem.applyModelViewMatrix();
+    DiffuseLighting.enableGuiDepthLighting();
+  }
+
+  private void runInFancyGraphicsMode(Runnable runnable) {
+    if (!MinecraftClient.isFabulousGraphicsOrBetter()) {
+      runnable.run();
+      return;
+    }
+
+    MinecraftClient client = MinecraftClient.getInstance();
+    SimpleOption<GraphicsMode> option = client.options.getGraphicsMode();
+    GraphicsMode originalGraphicsMode = option.getValue();
+    option.setValue(GraphicsMode.FANCY);
+    runnable.run();
+    option.setValue(originalGraphicsMode);
   }
 }
