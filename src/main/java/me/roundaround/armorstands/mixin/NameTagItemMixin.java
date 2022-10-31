4110b33036adb930ba360e18c5f8f5da0b88cd77
Evan Steinkerchner <esteinkerchner@gmail.com>
2022-10-31 17:48:02 -0400

    Only set name visible on name tag when player can edit stands

--- src/main/java/me/roundaround/armorstands/mixin/NameTagItemMixin.java
+++ src/main/java/me/roundaround/armorstands/mixin/NameTagItemMixin.java
@@ -5,11 +5,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import me.roundaround.armorstands.server.ArmorStandUsers;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.NameTagItem;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 
@@ -22,6 +24,10 @@ public abstract class NameTagItemMixin {
       LivingEntity entity,
       Hand hand,
       CallbackInfoReturnable<ActionResult> info) {
+    if (!(player instanceof ServerPlayerEntity) || !ArmorStandUsers.canEditArmorStands(player)) {
+      return;
+    }
+
     if (entity instanceof ArmorStandEntity) {
       entity.setCustomNameVisible(true);
     }
