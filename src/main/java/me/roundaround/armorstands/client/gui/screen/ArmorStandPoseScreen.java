6dc955e2501c6f991fe0893361f71ed44c28791c
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-02-19 22:41:32 -0500

    Switching computers agian

--- src/main/java/me/roundaround/armorstands/client/gui/screen/ArmorStandPoseScreen.java
+++ src/main/java/me/roundaround/armorstands/client/gui/screen/ArmorStandPoseScreen.java
@@ -2,35 +2,79 @@ package me.roundaround.armorstands.client.gui.screen;
 
 import java.util.List;
 
-import me.roundaround.armorstands.client.gui.widget.LabelWidget;
+import me.roundaround.armorstands.client.gui.widget.NavigationButton;
+import me.roundaround.armorstands.network.ArmorStandFlag;
 import me.roundaround.armorstands.screen.ArmorStandScreenHandler;
-import me.roundaround.armorstands.util.PosePart;
-import net.minecraft.client.gui.widget.TextFieldWidget;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
 
 public class ArmorStandPoseScreen
     extends AbstractArmorStandScreen {
   public static final Text TITLE = Text.translatable("armorstands.screen.pose");
   public static final int U_INDEX = 3;
 
-  private static final int MINI_BUTTON_WIDTH = 16;
-  private static final int MINI_BUTTON_HEIGHT = 16;
-  private static final int SCREEN_EDGE_PAD = 4;
-  private static final int BETWEEN_PAD = 2;
+  private static final int PADDING = 4;
+  private static final int HEADER_HEIGHT = 20;
+  private static final int FOOTER_HEIGHT = NavigationButton.HEIGHT + PADDING + NAV_BUTTON_BOTTOM_PADDING;
+
+  private final ArmorStandEntity previewStand;
 
   public ArmorStandPoseScreen(
       ArmorStandScreenHandler handler,
       ArmorStandEntity armorStand) {
     super(handler, TITLE, armorStand);
+
     this.supportsUndoRedo = true;
+    this.passEvents = false;
+
+    this.previewStand = initializePreviewStand();
+  }
+
+  private ArmorStandEntity initializePreviewStand() {
+    ArmorStandEntity previewStand = new ArmorStandEntity(
+        EntityType.ARMOR_STAND,
+        this.armorStand.world);
+
+    for (ArmorStandFlag flag : ArmorStandFlag.values()) {
+      if (flag == ArmorStandFlag.GRAVITY) {
+        flag.setValue(previewStand, true);
+        continue;
+      }
+      flag.setValue(previewStand, flag.getValue(armorStand));
+    }
+
+    previewStand.equipStack(EquipmentSlot.HEAD,
+        this.armorStand.getEquippedStack(EquipmentSlot.HEAD).copy());
+    previewStand.equipStack(EquipmentSlot.CHEST,
+        this.armorStand.getEquippedStack(EquipmentSlot.CHEST).copy());
+    previewStand.equipStack(EquipmentSlot.LEGS,
+        this.armorStand.getEquippedStack(EquipmentSlot.LEGS).copy());
+    previewStand.equipStack(EquipmentSlot.FEET,
+        this.armorStand.getEquippedStack(EquipmentSlot.FEET).copy());
+
+    previewStand.equipStack(EquipmentSlot.MAINHAND,
+        this.armorStand.getEquippedStack(EquipmentSlot.MAINHAND).copy());
+    previewStand.equipStack(EquipmentSlot.OFFHAND,
+        this.armorStand.getEquippedStack(EquipmentSlot.OFFHAND).copy());
+
+    previewStand.tick();
+
+    return previewStand;
+  }
+
+  @Override
+  public boolean shouldPause() {
+    return true;
   }
 
   @Override
   public void init() {
     super.init();
-
-    
 
     initNavigationButtons(List.of(
         ScreenFactory.create(
@@ -57,15 +101,26 @@ public class ArmorStandPoseScreen
             ArmorStandInventoryScreen.U_INDEX,
             ArmorStandInventoryScreen::new)));
   }
-  
-  private void addRowOfButtons(Text label, PosePart part, int y) {
-    addDrawable(LabelWidget.builder(
-        label,
-        SCREEN_EDGE_PAD,
-        y)
-        .alignedTop()
-        .justifiedLeft()
-        .shiftForPadding()
-        .build());
+
+  @Override
+  public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+    renderBackgroundTexture(0);
+
+    int size = MathHelper.floor(Math.min(
+        this.width / 2 - 4 * PADDING,
+        (this.height - HEADER_HEIGHT - FOOTER_HEIGHT - 3 * PADDING) / 2.5f));
+
+    int x = this.width * 3 / 4;
+    int y = this.height - FOOTER_HEIGHT - 3 * PADDING;
+
+    InventoryScreen.drawEntity(
+        x,
+        y,
+        size,
+        2f * (this.width / 2 - mouseX),
+        2f * (y - 50 - mouseY),
+        this.previewStand);
+
+    super.render(matrixStack, mouseX, mouseY, partialTicks);
   }
 }
