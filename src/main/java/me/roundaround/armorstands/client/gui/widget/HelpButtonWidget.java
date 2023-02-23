a933be837f12fd2bbebda18b78bab882d678973d
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-02-23 11:28:01 -0500

    Open mod's webpage on help button click

--- src/main/java/me/roundaround/armorstands/client/gui/widget/HelpButtonWidget.java
+++ src/main/java/me/roundaround/armorstands/client/gui/widget/HelpButtonWidget.java
@@ -1,9 +1,13 @@
 package me.roundaround.armorstands.client.gui.widget;
 
+import me.roundaround.armorstands.ArmorStandsMod;
 import me.roundaround.armorstands.client.gui.screen.AbstractArmorStandScreen;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ConfirmChatLinkScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 
 public class HelpButtonWidget extends IconButtonWidget<AbstractArmorStandScreen> {
   public HelpButtonWidget(
@@ -19,7 +23,22 @@ public class HelpButtonWidget extends IconButtonWidget<AbstractArmorStandScreen>
         16,
         wrapLines(Text.translatable("armorstands.help"), Math.max(200, parent.width / 2)),
         (button) -> {
-          // TODO: Link out to mod page
+          FabricLoader.getInstance().getModContainer(ArmorStandsMod.MOD_ID).ifPresent((mod) -> {
+            mod.getMetadata().getContact().get("homepage").ifPresent((url) -> {
+              // Clear currentScreen before changing to avoid closing the
+              // screen handler
+
+              client.currentScreen = null;
+              client.setScreen(new ConfirmChatLinkScreen((confirmed) -> {
+                if (confirmed) {
+                  Util.getOperatingSystem().open(url);
+                }
+                client.currentScreen = null;
+                client.setScreen(parent);
+              }, url, false));
+            });
+          });
+
         });
   }
 
