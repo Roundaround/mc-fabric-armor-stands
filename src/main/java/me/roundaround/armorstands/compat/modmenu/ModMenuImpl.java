ba11ba872484059ec03db69504b0a8f0c58341c1
Roundaround <roundaroundna@gmail.com>
2025-10-02 11:52:42 -0400

    Re-implemented ModMenu integration

--- src/main/java/me/roundaround/armorstands/compat/modmenu/ModMenuImpl.java
+++ src/main/java/me/roundaround/armorstands/compat/modmenu/ModMenuImpl.java
@@ -1,19 +1,19 @@
 package me.roundaround.armorstands.compat.modmenu;
 
-//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-//import com.terraformersmc.modmenu.api.ModMenuApi;
-//import me.roundaround.armorstands.ArmorStandsMod;
-//import me.roundaround.armorstands.client.ClientSideConfig;
-//import me.roundaround.armorstands.roundalib.client.gui.screen.ConfigScreen;
-//import me.roundaround.gradle.api.annotation.Entrypoint;
-//
-//@Entrypoint(Entrypoint.MOD_MENU)
-//public class ModMenuImpl implements ModMenuApi {
-//  @Override
-//  public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//    return (parent) -> {
-//      ClientSideConfig config = ClientSideConfig.getInstance();
-//      return config.isApplicable() ? new ConfigScreen(parent, ArmorStandsMod.MOD_ID, config) : null;
-//    };
-//  }
-//}
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.roundaround.armorstands.ArmorStandsMod;
+import me.roundaround.armorstands.client.ClientSideConfig;
+import me.roundaround.armorstands.roundalib.client.gui.screen.ConfigScreen;
+import me.roundaround.gradle.api.annotation.Entrypoint;
+
+@Entrypoint(Entrypoint.MOD_MENU)
+public class ModMenuImpl implements ModMenuApi {
+  @Override
+  public ConfigScreenFactory<?> getModConfigScreenFactory() {
+    return (parent) -> {
+      ClientSideConfig config = ClientSideConfig.getInstance();
+      return config.isApplicable() ? new ConfigScreen(parent, ArmorStandsMod.MOD_ID, config) : null;
+    };
+  }
+}
