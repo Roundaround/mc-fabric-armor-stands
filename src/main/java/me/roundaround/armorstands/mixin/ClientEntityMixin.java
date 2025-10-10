ed4fc5e75a2e50d059683a87e79e1556024f86de
Roundaround <roundaroundna@gmail.com>
2025-10-10 10:31:09 -0400

    Fix injected interface for Entity only applying client-side

---
+++ src/main/java/me/roundaround/armorstands/mixin/ClientEntityMixin.java
@@ -0,0 +1,62 @@
+package me.roundaround.armorstands.mixin;
+
+import me.roundaround.armorstands.interfaces.EntityPosition;
+import me.roundaround.armorstands.screen.ArmorStandScreenHandler;
+import me.roundaround.gradle.api.annotation.MixinEnv;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+
+@Mixin(Entity.class)
+@MixinEnv(MixinEnv.CLIENT)
+public abstract class ClientEntityMixin implements EntityPosition {
+  @Inject(
+      method = "updateTrackedPositionAndAngles(Ljava/util/Optional;Ljava/util/Optional;Ljava/util/Optional;)V",
+      at = @At(value = "HEAD"),
+      cancellable = true
+  )
+  public void updateTrackedPositionAndAngles(
+      Optional<Vec3d> pos,
+      Optional<Float> yaw,
+      Optional<Float> pitch,
+      CallbackInfo info
+  ) {
+    if (!(this.self() instanceof ArmorStandEntity self)) {
+      return;
+    }
+
+    World world = self.getEntityWorld();
+    if (!world.isClient()) {
+      return;
+    }
+
+    MinecraftClient client = MinecraftClient.getInstance();
+    if (client.player == null) {
+      return;
+    }
+
+    ScreenHandler rawScreenHandler = client.player.currentScreenHandler;
+    if (!(rawScreenHandler instanceof ArmorStandScreenHandler screenHandler)) {
+      return;
+    }
+
+    if (screenHandler.getArmorStand() == self) {
+      info.cancel();
+    }
+  }
+
+  @Unique
+  private Entity self() {
+    return (Entity) (Object) this;
+  }
+}
--- src/main/java/me/roundaround/armorstands/mixin/EntityMixin.java
+++ src/main/java/me/roundaround/armorstands/mixin/EntityMixin.java
@@ -1,26 +1,13 @@
 package me.roundaround.armorstands.mixin;
 
 import me.roundaround.armorstands.interfaces.EntityPosition;
-import me.roundaround.armorstands.screen.ArmorStandScreenHandler;
-import me.roundaround.gradle.api.annotation.MixinEnv;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Optional;
 
 @Mixin(Entity.class)
-@MixinEnv(MixinEnv.CLIENT)
-public abstract class EntityMixin implements EntityPosition {
+public class EntityMixin implements EntityPosition {
   @Shadow
   private Vec3d pos;
 
@@ -28,45 +15,5 @@ public abstract class EntityMixin implements EntityPosition {
   public Vec3d armorstands$getPos() {
     // Copy so we don't accidentally modify the coords individually inside the pos object
     return new Vec3d(this.pos.x, this.pos.y, this.pos.z);
-  }
-
-  @Inject(
-      method = "updateTrackedPositionAndAngles(Ljava/util/Optional;Ljava/util/Optional;Ljava/util/Optional;)V",
-      at = @At(value = "HEAD"),
-      cancellable = true
-  )
-  public void updateTrackedPositionAndAngles(
-      Optional<Vec3d> pos,
-      Optional<Float> yaw,
-      Optional<Float> pitch,
-      CallbackInfo info
-  ) {
-    if (!(this.self() instanceof ArmorStandEntity self)) {
-      return;
-    }
-
-    World world = self.getEntityWorld();
-    if (!world.isClient()) {
-      return;
-    }
-
-    MinecraftClient client = MinecraftClient.getInstance();
-    if (client.player == null) {
-      return;
-    }
-
-    ScreenHandler rawScreenHandler = client.player.currentScreenHandler;
-    if (!(rawScreenHandler instanceof ArmorStandScreenHandler screenHandler)) {
-      return;
-    }
-
-    if (screenHandler.getArmorStand() == self) {
-      info.cancel();
-    }
-  }
-
-  @Unique
-  private Entity self() {
-    return (Entity) (Object) this;
   }
 }
